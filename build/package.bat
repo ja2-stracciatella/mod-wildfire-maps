656a356e5329f18d217f624f66005828c1b7eab2
ja2-derek <sfod_csy@hotmail.com>
2020-04-12 05:05:21 +0800

    Creating package for distribution

--- build/package.bat
+++ build/package.bat
@@ -1,7 +1,18 @@
 REM pip install pypiwin32 pefile 
 @ECHO ON
-del *.spec
+DEL *.spec
 pyinstaller ..\src\install_wildfire_maps.py ^
     --distpath ..\dist ^
     --paths ..\lib\ja2-open-toolset ^
     --onefile --console
+
+CD ..
+RMDIR dist\wildfire-maps\ /s /q
+ROBOCOPY assets\wildfire-maps\ dist\wildfire-maps\ /MIR
+
+COPY README.txt       dist\wildfire-maps\
+COPY KNOWN_ISSUES.txt dist\wildfire-maps\
+MOVE  dist\install_wildfire_maps.exe dist\wildfire-maps\
+
+CD build
+
--- build/package.sh
+++ build/package.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
+rm *.spec
 pyinstaller ../src/install_wildfire_maps.py \
     --distpath ../dist \
     --paths ../lib/ja2-open-toolset \
-    --onefile --console
+    --onefile
+
+pushd ..
+rm -rf dist/wildfire-maps/
+mkdir -p dist/wildfire-maps
+mv dist/install_wildfire_maps dist/wildfire-maps/
+cp -R assets/wildfire-maps/ dist/
+cp README.txt KNOWN_ISSUES.txt  dist/wildfire-maps/
+
+pushd dist
+tar zcf wildfire-maps.tar.gz wildfire-maps/
