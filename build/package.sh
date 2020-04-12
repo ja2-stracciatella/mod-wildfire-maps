06b2f02044e75a41b4d80527e8609cfae38136ce
ja2-derek <sfod_csy@hotmail.com>
2020-04-12 15:01:57 +0800

    Setting up TravisCI to build dist packages

--- build/package.sh
+++ build/package.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+# This is packaging script assumes an environment like TravisCI.
 
-rm *.spec
-pyinstaller ../src/install_wildfire_maps.py \
-    --distpath ../dist \
-    --paths ../lib/ja2-open-toolset \
+OS=${TRAVIS_OS_NAME-unknown}
+TAG=${TRAVIS_TAG-untagged}
+
+BASE_DIR="$(dirname "$BASH_SOURCE")/.."
+DIST_DIR="$BASE_DIR/dist/wildfire-maps"
+
+# Create executable binary
+pyinstaller $BASE_DIR/src/install_wildfire_maps.py \
+    --distpath $BASE_DIR/dist/ \
+    --paths $BASE_DIR/lib/ja2-open-toolset \
     --onefile
 
-pushd ..
-rm -rf dist/wildfire-maps/
-mkdir -p dist/wildfire-maps
-mv dist/install_wildfire_maps dist/wildfire-maps/
-cp -R assets/wildfire-maps/ dist/
-cp README.txt KNOWN_ISSUES.txt  dist/wildfire-maps/
+# Create dist package
+rm    -rf $DIST_DIR/
+mkdir -p  $DIST_DIR/
+mv    $BASE_DIR/dist/install_wildfire_maps*            $DIST_DIR/
+cp -R $BASE_DIR/assets/wildfire-maps/*                 $DIST_DIR/
+cp    $BASE_DIR/README.txt $BASE_DIR/KNOWN_ISSUES.txt  $DIST_DIR/
 
-pushd dist
-tar zcf wildfire-maps.tar.gz wildfire-maps/
+pushd $BASE_DIR/dist
+if [ "$OS" = "windows" ]; then
+    7z  a   wildfire-maps_${TAG}-${OS}.zip    wildfire-maps/
+else
+    tar zcf wildfire-maps_${TAG}-${OS}.tar.gz wildfire-maps/
+fi
+popd
