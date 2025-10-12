bedaca8255648cf68eaebcd961a356cf8b44b7ce
ja2-derek <63151803+ja2-derek@users.noreply.github.com>
2025-10-13 06:21:01 +0800

    try to fix build

--- build/package.sh
+++ build/package.sh
@@ -10,6 +10,8 @@ DIST_DIR="$BASE_DIR/dist/wildfire-maps"
 # Create executable binary
 pyinstaller $BASE_DIR/src/install_wildfire_maps.py \
     --hidden-import='pkg_resources.py2_warn' \
+    --hidden-import='PIL' \
+    --collect-all='PIL' \
     --distpath $BASE_DIR/dist/ \
     --onefile
 
