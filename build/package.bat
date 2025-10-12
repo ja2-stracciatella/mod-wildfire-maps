2f04ce0f3f286c0a6490671a5540249392b2e0ac
ja2-derek <63151803+ja2-derek@users.noreply.github.com>
2025-10-12 20:21:01 +0800

    try to fix build

--- build/package.bat
+++ build/package.bat
@@ -1,7 +1,12 @@
-REM pip install pypiwin32 pefile 
 @ECHO ON
 DEL *.spec
 pyinstaller ..\src\install_wildfire_maps.py ^
     --distpath ..\dist ^
+    --hidden-import='pkg_resources.py2_warn' ^
+    --hidden-import='PIL._imaging' ^
+    --collect-all='PIL' ^
     --paths ..\lib\ja2-open-toolset ^
     --onefile --console
+
+ECHO "Smoke-test the executable"
+..\dist\install_wildfire_maps.exe -h
